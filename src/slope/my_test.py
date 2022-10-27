958446f742e0dffd3fac5f11b3a12fadbabf2d78
Ivan Hejný <ivan.hejny@gmail.com>
2022-10-27 19:51:20 +0200

    trying to understand functions prox_slope in utils and pgd_slope in solvers

--- src/slope/my_test.py
+++ src/slope/my_test.py
@@ -1,11 +1,19 @@
 import numpy as np
+import math
 
-from src.slope.solvers import pgd_slope
+from src.slope.solvers import pgd_slope, pgd_slope_without_n
 from src.slope.utils import prox_slope
 X = np.array([[1.0, 0.0], [0.0, 1.0]])
 y = np.array([5.0, 4.0])
 lambdas = np.array([3.0, 1.0])
 
-print(prox_slope(y,lambdas))# solves 0.5||y-b||^2+ <lambda, b_i> subject to b_1>=b_2>=0
-print(pgd_slope(X, y, lambdas, fit_intercept=False, gap_tol=1e-6, max_it=10_000, verbose=False,)) # solves 0.5||y-Xb||^2+ <lambda,|b|_(i)> ??
+print("prox_slope:", prox_slope(y,lambdas))# solves 0.5||y-b||^2+ <lambda, b_i> subject to b_1>=b_2>=0
 
+print("pgd_slope:", pgd_slope(X, y, lambdas, fit_intercept=False, gap_tol=1e-6, max_it=10_000, verbose=False,)) # solves 0.5/n*||y-Xb||^2+ <lambda,|b|_(i)>
+
+print("pgd_slope_without_n:", pgd_slope_without_n(X, y, lambdas, fit_intercept=False, gap_tol=1e-6, max_it=10_000, verbose=False,)) # solves 0.5*||y-Xb||^2+ <lambda,|b|_(i)>
+
+
+
+#a=math.sqrt(2)
+#print("pgd_slope:", pgd_slope(a*X, a*y, lambdas, fit_intercept=False, gap_tol=1e-6, max_it=10_000, verbose=False,)) # solves 0.5||y-Xb||^2+ <lambda,|b|_(i)>
--- src/slope/solvers.py
+++ src/slope/solvers.py
@@ -54,3 +54,56 @@ def pgd_slope(
 
     #return dict(beta=beta, intercept=intercept, primals=primals, duals=duals, gaps=gaps)
     return dict(beta=beta, intercept=intercept)
+
+
+def pgd_slope_without_n(
+    X,
+    y,
+    lambdas,
+    fit_intercept=True,
+    gap_tol=1e-6,
+    max_it=10_000,
+    verbose=False,
+):
+    n, p = X.shape
+
+    lambdas = lambdas/n
+    residual = y.copy()
+    beta = np.zeros(p)
+    intercept = 0.0
+
+    primals, duals, gaps = [], [], []
+
+    if sparse.issparse(X):
+        L = sparse.linalg.svds(X, k=1)[1][0] ** 2 / n
+    else:
+        L = norm(X, ord=2) ** 2 / n
+
+    for it in range(max_it):
+        beta = prox_slope(beta + (X.T @ residual) / (L * n), lambdas / L)
+
+        if fit_intercept:
+            intercept = np.mean(residual)
+
+        residual = y - X @ beta - intercept
+
+        theta = residual / n
+        theta /= max(1, dual_norm_slope(X, theta, lambdas))
+
+        primal = 0.5 * norm(residual) ** 2 / n + sl1_norm(beta, lambdas)
+        dual = 0.5 * (norm(y) ** 2 - norm(y - theta * n) ** 2) / n
+        gap = primal - dual
+
+        primals.append(primal)
+        duals.append(primal)
+        gaps.append(gap)
+
+        if verbose:
+            print(f"epoch: {it + 1}, loss: {primal}, gap: {gap:.2e}")
+
+        if gap < gap_tol:
+            break
+
+    #return dict(beta=beta, intercept=intercept, primals=primals, duals=duals, gaps=gaps)
+    return dict(beta=beta, intercept=intercept)
+
